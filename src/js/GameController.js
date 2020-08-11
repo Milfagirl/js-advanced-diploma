ef3aae315273d74e325ecb04f1a7cf143d91e4ae
Svetlana Ryabova <ganyim@inbox.ru>
2020-08-12 01:04:26 +0500

    check class Team

--- src/js/GameController.js
+++ src/js/GameController.js
@@ -13,52 +13,22 @@ import { gamestate } from './GameState.js';
 import GamePlay from './GamePlay.js';
 import cursors from './cursors.js';
 import field from './field.js';
+import team from './Team.js';
 
 export default class GameController {
   constructor(gamePlay, stateService) {
     this.gamePlay = gamePlay;
     this.stateService = stateService;
-    this.allPositions = [];
-  }
-
-  get takecharacter() {
-    return this.allPositions;
-    // геттер, срабатывает при чтении obj.propName
-  }
-
-  set takecharacter(value) {
-    this.allPositions = value;
-    // сеттер, срабатывает при записи obj.propName = value
   }
 
   init() {
     this.gamePlay.drawUi(themes.prairie);
-    const types = [Bowman, Daemon, Magician, Swordsman, Undead, Vampire];
-    const team1 = generateTeam(types, 4, 2); // персонажи для команды 1
-    const positions1 = []; // массив персонажей команды 1 с указанием позиции
-    const stay1 = [];
-    for (let i = 0; i < 8; i++) {
-      stay1.push(i * 8, i * 8 + 1);
-    }
-    team1.forEach((item) => {
-      positions1.push(new PositionedCharacter(item, stay1[Math.floor(Math.random() * stay1.length)]));
-    });
-    const team2 = generateTeam(types, 4, 2); // персонажи для команды 2
-    const positions2 = []; // массив персонажей команды 2 с указанием позиции
-    const stay2 = [];
-    for (let i = 0; i < 8; i++) {
-      stay2.push(i * 8 + 6, i * 8 + 7);
-    }
-    team2.forEach((item) => {
-      positions2.push(new PositionedCharacter(item, stay2[Math.floor(Math.random() * stay2.length)]));
-    });
-    this.takecharacter = positions1.concat(positions2); // формируем массив PositionedCharacter (персонаж, индекс ячейки)
-    this.gamePlay.redrawPositions(this.allPositions); // отрисовка персонажей на игровом поле
+    this.gamePlay.redrawPositions(team.takecharacter); // отрисовка персонажей на игровом поле
     this.gamePlay.addCellEnterListener(this.onCellEnter); // событие - наведение курсора мыши
     this.gamePlay.addCellClickListener(this.onCellClick); // событие - клик курсора мыши
     // TODO: add event listeners to gamePlay events
     // TODO: load saved stated from stateService
-    console.log(this.allPositions);
+    console.log(team.takecharacter);
     console.log(field);
   }
 
@@ -68,9 +38,9 @@ export default class GameController {
     gamestate.in = false; // персонаж не выбран
     this.gamePlay.deselectCell(gamestate.from);
     if (character.length > 0) {
-      this.allPositions.forEach((item) => {
+      team.takecharacter.forEach((item) => {
         if (item.position === index) {
-          if (this.allPositions.indexOf(item) === 2 || this.allPositions.indexOf(item) === 3) {
+          if (team.takecharacter.indexOf(item) === 2 || team.takecharacter.indexOf(item) === 3) {
             GamePlay.showError('Выбирайте персонажа из своей команды!');
           } else {
             this.gamePlay.selectCell(index); // выделение выбранного персонажа
@@ -89,7 +59,7 @@ export default class GameController {
     const character = cell.item(index).childNodes;
     this.gamePlay.setCursor(cursors.auto);
     if (character.length > 0) {
-      this.allPositions.forEach((item) => {
+      team.takecharacter.forEach((item) => {
         if (item.position === index) {
           if (gamestate.in) {
             this.gamePlay.setCursor(cursors.pointer);
--- src/js/Team.js
+++ src/js/Team.js
@@ -1,4 +1,51 @@
 // класс для команды (набор персонажей), представляющих компьютер и игрока
-export default class Team {
+import Bowman from './childrens/Bowman.js';
+import Daemon from './childrens/Daemon.js';
+import Magician from './childrens/Magician.js';
+import Swordsman from './childrens/Swordsman.js';
+import Undead from './childrens/Undead.js';
+import Vampire from './childrens/Vampire.js';
+import generateTeam from './generators.js';
+import PositionedCharacter from './PositionedCharacter.js';
 
+export class Team {
+  constructor() {
+    this.allPositions = [];
+    this.takeTeam();
+  }
+
+  get takecharacter() {
+    return this.allPositions;
+    // геттер, срабатывает при чтении obj.propName
+  }
+
+  set takecharacter(value) {
+    this.allPositions = value;
+    // сеттер, срабатывает при записи obj.propName = value
+  }
+
+  takeTeam() {
+    const types = [Bowman, Daemon, Magician, Swordsman, Undead, Vampire];
+    const team1 = generateTeam(types, 4, 2); // персонажи для команды 1
+    const positions1 = []; // массив персонажей команды 1 с указанием позиции
+    const stay1 = [];
+    for (let i = 0; i < 8; i++) {
+      stay1.push(i * 8, i * 8 + 1);
+    }
+    team1.forEach((item) => {
+    positions1.push(new PositionedCharacter(item, stay1[Math.floor(Math.random() * stay1.length)]));
+    });
+    const team2 = generateTeam(types, 4, 2); // персонажи для команды 2
+    const positions2 = []; // массив персонажей команды 2 с указанием позиции
+    const stay2 = [];
+    for (let i = 0; i < 8; i++) {
+      stay2.push(i * 8 + 6, i * 8 + 7);
+    }
+    team2.forEach((item) => {
+    positions2.push(new PositionedCharacter(item, stay2[Math.floor(Math.random() * stay2.length)]));
+    });
+    this.takecharacter = positions1.concat(positions2);
+  }
 }
+const team = new Team();
+export default team;
